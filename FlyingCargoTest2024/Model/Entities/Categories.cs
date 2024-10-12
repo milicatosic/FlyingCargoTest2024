59c57c86eb05bb5091e5cb4f3838f6f9b40b2716
Milica Tošić <milicica.tosic@gmail.com>
2024-10-12 18:39:48 +0200

    Initial commit

---
+++ FlyingCargoTest2024/Model/Entities/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entities
+{
+    [Table("Categories")]
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ FlyingCargoTest2024/Model/Entities/ProductCategories.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entities
+{
+    [Table("ProductCategories")]
+    public class ProductCategories
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
